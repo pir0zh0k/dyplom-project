4363a009d9cba29a6e13fb31ab1ec1693c75bb69
Pavel Zalyubovin <p.zalyubovin@intensa.ru>
2024-05-04 16:46:24 +0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Intensa-Chat')
+    .setDescription('Chat API for Intensa-Chat-App')
+    .setVersion('1.0')
+    .addTag('Intensa-Chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(4200);
 }
 bootstrap();
